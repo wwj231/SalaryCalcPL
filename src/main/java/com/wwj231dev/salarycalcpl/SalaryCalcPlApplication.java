0f7686562242df46e89b91343d5e9fc949496695
Wladyslaw Wiktor Janicki <wwj231@gmail.com>
2021-10-14 09:49:55 +0200

    Initial Commit

---
+++ src/main/java/com/wwj231dev/salarycalcpl/SalaryCalcPlApplication.java
@@ -0,0 +1,13 @@
+package com.wwj231dev.salarycalcpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCalcPlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryCalcPlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wwj231dev/salarycalcpl/SalaryCalcPlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwj231dev.salarycalcpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalcPlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
